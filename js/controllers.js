72747c9d2a172d48e844c23bebcfc6760e8af184
unknown <pablo.felitti@TDIBAFELITTIP.tdi.corp.jwt.com>
2015-02-13 15:11:40 -0300

    model refactor

--- js/controllers.js
+++ js/controllers.js
@@ -1,25 +1,11 @@
 (function () {
     var app = angular.module("main", []);
 
-    var carTypes = [{
-        name: "Cars",
-        models: [{name: "MKZ", canPurchase: true, price: 15000.35, img: "img/mkz.png"}, {
-            name: "MKS",
-            canPurchase: true,
-            price: 22000.40,
-            img: "img/mks.png"
-        }]
-    }, {
-        name: "Crossovers",
-        models: [{name: "MKC", canPurchase: false, price: 10000.00, img: "img/mkc.png"}, {
-            name: "MKX",
-            canPurchase: false,
-            price: 8500.25,
-            img: "img/mkx.png"
-        }, {
-            name: "MKT", canPurchase: true, price: 3500.50, img: "img/mkt.png"
-        }]
-    }];
+    var carModels = [new Model("MKZ", true, 15000.35, "img/mkz.png"), new Model("MKS", true, 22000.40, "img/mks.png")];
+    var crossoverModels = [new Model("MKC", false, 10000.00, "img/mkc.png"), new Model("MKX", false, 8500.25, "img/mkx.png"), new Model("MKT", true, 3500.50, "img/mkt.png")];
+    var carType = new CarType("Cars", carModels);
+    var crossoverType = new CarType("Crossovers", crossoverModels);
+    var carTypes = [carType, crossoverType];
 
     app.controller("MainController", function () {
         this.carTypes = carTypes;
@@ -38,4 +24,28 @@
             }
         }
     });
-})();
+
+    app.controller("ReviewController", function () {
+        this.review = {};
+        this.addReview = function (model) {
+            model.addReview(this.review);
+        }
+    });
+})
+();
+
+function CarType(name, models) {
+    this.name = name;
+    this.models = models;
+}
+
+function Model(name, canPurchase, price, img) {
+    this.name = name;
+    this.canPurchase = canPurchase;
+    this.price = price;
+    this.img = img;
+
+    this.addReview = function (review) {
+        this.reviews.push(review);
+    }
+}
