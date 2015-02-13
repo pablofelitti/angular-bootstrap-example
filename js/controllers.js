f5e53eeaf1d7ee82b070a9cd6ecfd733c718a405
unknown <pablo.felitti@TDIBAFELITTIP.tdi.corp.jwt.com>
2015-02-13 11:31:46 -0300

    Images added

--- js/controllers.js
+++ js/controllers.js
@@ -3,11 +3,21 @@
 
     var carTypes = [{
         name: "Cars",
-        models: [{name: "MKZ", canPurchase: true}, {name: "MKS", canPurchase: true}]
+        models: [{name: "MKZ", canPurchase: true, price: 15000.35, img: "img/mkz.png"}, {
+            name: "MKS",
+            canPurchase: true,
+            price: 22000.40,
+            img: "img/mks.png"
+        }]
     }, {
         name: "Crossovers",
-        models: [{name: "MKC", canPurchase: false}, {name: "MKX", canPurchase: false}, {
-            name: "MKT", canPurchase: true
+        models: [{name: "MKC", canPurchase: false, price: 10000.00, img: "img/mkc.png"}, {
+            name: "MKX",
+            canPurchase: false,
+            price: 8500.25,
+            img: "img/mkx.png"
+        }, {
+            name: "MKT", canPurchase: true, price: 3500.50, img: "img/mkt.png"
         }]
     }];
 
