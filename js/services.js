0d06c9ee707e49dd6b22a0b3d6db23b69f8521ed
Pablo Felitti <pablofelitti@Pablos-MacBook-Pro.local>
2015-02-16 20:50:15 -0300

    ajax json local file added

--- js/services.js
+++ js/services.js
@@ -1,9 +1,19 @@
 (function () {
     var app = angular.module("services", []);
 
-    app.service("carTypeService", function () {
+    app.service("carTypeService", function ($http) {
         this.loadCarTypes = function () {
-            var carModels = [new Model("MKZ", true, 15000.35, "img/mkz.png"), new Model("MKS", true, 22000.40, "img/mks.png")];
+
+            var carModels = [];
+
+            $http.get("src/car-models.json").success(function (result) {
+                angular.forEach(result, function (value) {
+                    carModels.push(new Model(value.name, value.canPurchase, value.price, value.img));
+                });
+            }).error(function () {
+                alert("error loading car models");
+            });
+
             var crossoverModels = [new Model("MKC", false, 10000.00, "img/mkc.png"), new Model("MKX", false, 8500.25, "img/mkx.png"), new Model("MKT", true, 3500.50, "img/mkt.png")];
             var carType = new CarType("Cars", carModels);
             var crossoverType = new CarType("Crossovers", crossoverModels);
